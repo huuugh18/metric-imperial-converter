1e341ea946e71afb851be0b4b75066a68c826f59
spam-man <reprograma@protonmail.com>
2018-09-13 15:34:04 -0300

    Beginning changes

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const path        = require('path');
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/public', express.static(process.cwd() + '/public'));
+const app = express();
+
+app.use('/public', express.static(path.join(process.cwd(), 'public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
@@ -21,15 +23,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(path.join(process.cwd(), 'views', 'index.html'));
   });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -38,15 +40,15 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(port, function () {
+  console.log("Listening on port " + port);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
       } catch(e) {
-        var error = e;
+        let error = e;
           console.log('Tests are not valid:');
           console.log(error);
       }
---
+++ test/_convertHandler.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const convertHandler = require('../controllers/convertHandler');
+
+describe('Conversion Handler', () => {
+
+
+  xit('should convert units', () => {});
+});
