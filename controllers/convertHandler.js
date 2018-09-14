436c757a7a70f9ff77857d0cc7bd30df8bda9af2
spam-man <reprograma@protonmail.com>
2018-09-13 21:05:05 -0300

    Adds convert function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,52 +1,86 @@
-/*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
+class ConvertHandler {
 
-function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
-  };
+  constructor () {
 
-  this.spellOutUnit = function(unit) {
-    var result;
-    
+  }
+
+  getNum (input) {
+    let result;
+
     return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
+  }
+
+  getUnit (input) {
+    let result;
+
+    return result;
+  }
+
+  getReturnUnit (initUnit) {
+    let result;
+
+    return result;
+  }
+
+  spellOutUnit (unit) {
+    let result;
+
+    return result;
+  }
+
+  convert (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
+    let result = Number(initNum);
+    let numberIsValid = true;
+    let unitIsValid = true;
+    if (isNaN(result)) {
+      numberIsValid = false;
+    }
+
+    switch (initUnit) {
+      case 'gal':
+        result *= galToL;
+        break;
+      case 'L':
+        result /= galToL;
+        break;
+      case 'lbs':
+        result *= lbsToKg;
+        break;
+      case 'Kg':
+        result /= lbsToKg;
+        break;
+      case 'mi':
+        result *= miToKm;
+        break;
+      case 'Km':
+        result /= miToKm;
+        break;
+      default:
+        unitIsValid = false;
+    }
+
+    if (!numberIsValid && !unitIsValid) {
+      throw new Error('invalid number and unit');
+    } else if (!numberIsValid) {
+      throw new Error('invalid number');
+    } else if (!unitIsValid) {
+      throw new Error('invalid unit');
+    }
+
+    return Number(result.toFixed(5));
+  }
+
+  getString (initNum, initUnit, returnNum, returnUnit) {
+    let result;
+
     return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
-  };
-  
-}
+  }
+
+  }
+
+
 
 module.exports = ConvertHandler;
--- test/_convertHandler.js
+++ test/_convertHandler.js
@@ -1,4 +1,5 @@
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
 const ConvertHandler = require('../controllers/convertHandler');
 
 const convertHandler = new ConvertHandler();
@@ -6,5 +7,58 @@ const convertHandler = new ConvertHandler();
 describe('Conversion Handler', () => {
 
 
-  xit('should convert units', () => {});
+  describe('Function convertHandler.convert(num, unit)', () => {
+
+    it('Gal to L', function(done) {
+      let input = [5, 'gal'];
+      let expected = 18.9271;
+      let result = convertHandler.convert(input[0], input[1]);
+      expect(result).to.be.closeTo(expected, 0.1); //0.1 tolerance
+      done();
+    });
+
+    it('L to Gal', function(done) {
+      let input = [10, 'L'];
+      let expected = 2.64172;
+      let result = convertHandler.convert(input[0], input[1]);
+      expect(result).to.be.closeTo(expected, 0.1); //0.1 tolerance
+      done();
+    });
+
+    it('Mi to Km', function(done) {
+      let input = [23, 'mi'];
+      let expected = 37.01482;
+      let result = convertHandler.convert(input[0], input[1]);
+      expect(result).to.be.closeTo(expected, 0.1); //0.1 tolerance
+      done();
+    });
+
+    it('Km to Mi', function(done) {
+      let input = [33, 'Km'];
+      let expected = 20.50530;
+      let result = convertHandler.convert(input[0], input[1]);
+      expect(result).to.be.closeTo(expected, 0.1); //0.1 tolerance
+      done();
+    });
+
+    it('Lbs to Kg', function(done) {
+      let input = [100, 'lbs'];
+      let expected = 45.3592;
+      let result = convertHandler.convert(input[0], input[1]);
+      expect(result).to.be.closeTo(expected, 0.1); //0.1 tolerance
+      done();
+    });
+
+    it('Kg to Lbs', function(done) {
+      let input = [300, 'Kg'];
+      let expected = 661.38732;
+      let result = convertHandler.convert(input[0], input[1]);
+      expect(result).to.be.closeTo(expected, 0.1); //0.1 tolerance
+      done();
+    });
+
+  });
+
+
+
 });
