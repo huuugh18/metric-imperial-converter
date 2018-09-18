b4a480f965581f19bfc0f4e1b94a9b381b661562
spam-man <reprograma@protonmail.com>
2018-09-18 17:13:47 -0300

    Functional tests ok

--- controllers/sanitize.js
+++ controllers/sanitize.js
@@ -1,14 +1,12 @@
 const arrayWrap = require('arraywrap');
 
 function sanitize (input) {
-
-  // Guard against multiple inputs converting all inputs to arrays
+  // Convert all inputs to arrays
   let inputArray = arrayWrap(input);
-  // Get only the first input, ignoring the rest
+  // Get only the first item, ignoring the rest
   let sanInput = inputArray[0] ? inputArray[0] : '';
-
+  // Remove spaces
   sanInput = sanInput.replace(/\s+/g, '');
-
   return sanInput.toLowerCase();
 }
 module.exports = sanitize;
--- test/_sanitize.js
+++ test/_sanitize.js
@@ -4,9 +4,28 @@ const sanitize = require('../controllers/sanitize');
 
 describe('Input Sanitizier', function() {
 
-  it('should sanitize inputs', function() {
+  it('removes spaces between number and unit', function() {
     let input = '1 L';
     let result = sanitize(input);
     expect(result).to.equal('1l');
   });
+
+  it('ignores query arrays', function() {
+    let input = ['1 KG', '1 mi', 'ABCD'];
+    let result = sanitize(input);
+    expect(result).to.equal('1kg');
+  });
+
+  it('returns a blank string if the input is empty', function() {
+    let input = null;
+    let result = sanitize(input);
+    expect(result).to.equal('');
+  });
+
+  it('converts input strings to lower case', function() {
+    let input = 'ABCD';
+    let result = sanitize(input);
+    expect(result).to.equal('abcd');
+  });
+
 });
